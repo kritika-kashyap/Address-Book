e191cff959e4d70dfd0cc7878edab37e0e738ce3
kritika-kashyap <135224599+kritika-kashyap@users.noreply.github.com>
2024-12-01 19:29:20 +0530

    Few changes

--- Main.java
+++ Main.java
@@ -7,10 +7,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Address book");
         do{
-            System.out.println("Enter 1 to create contact");
+            System.out.println("Enter 1 to create contact \n Enter 2 to display all contacts"); 
             int input = sc.nextInt();
-            switch (input){
+
+            switch(input) {
                 case 1 -> a1.createContact();
+                case 2 -> a1.display();
                 default -> System.out.println("Wrong input");
             }
             System.out.println("Enter 0 to exit");
